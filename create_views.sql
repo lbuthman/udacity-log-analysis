34718fc91c177816726d61a4a734671450810c79
lbuthman <lbuthman@gmail.com>
2017-08-15 18:34:57 -0500

    Create easy way for user to load views into database

---
+++ create_views.sql
@@ -0,0 +1,12 @@
+CREATE VIEW author_article AS SELECT name, slug FROM authors, articles
+WHERE authors.id = articles.author;
+
+CREATE VIEW view_errors AS SELECT date_trunc('day', log.time) AS day, COUNT(*)
+FROM log WHERE status = '404 NOT FOUND' GROUP BY day;
+
+CREATE VIEW view_total AS SELECT date_trunc('day', log.time) AS day, COUNT(*)
+FROM log GROUP BY day;
+
+CREATE VIEW error_percentage AS SELECT view_total.day,
+(view_errors.count::decimal / view_total.count) * 100 AS percentage
+FROM view_total, view_errors WHERE view_total.day = view_errors.day;
